d9531f45b6acc54a86cd51dd32568c4a26406afa
stelis-stelico <121448105+stelis-stelico@users.noreply.github.com>
2023-04-03 15:19:14 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,37 +3,13 @@ var buttons = document.querySelectorAll('button');
 var resultDisplay = document.getElementById('result');
 
 // Define the variables as an empty string
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
+let currentInput = '';
+let storedInput = '';
+let previousResult = '';
 
 // Define a function to update the result display
 function updateDisplay(value) {
   resultDisplay.innerText = value;
-}
-
-// Define a function to perform the calculation
-function calculate() {
-
-  // Convert the variables to numbers
-  const num1 = parseFloat(firstNumber);
-  const num2 = parseFloat(secondNumber);
-
-    // Performing the calculations based on the operator clicked
-    if (operator === '+') {
-      updateDisplay(num1 + num2);
-    } else if (operator === '-') {
-      updateDisplay(num1 - num2);
-    } else if (operator === 'x') {
-      updateDisplay(num1 * num2);
-    } else if (operator === '/') {
-      updateDisplay(num1 / num2);
-    }
-
-  // Reset the variables to their initial state
-  firstNumber = '';
-  secondNumber = '';
-  operator = '';
 }
 
 // Looping through all the buttons and adding click event listeners
@@ -41,33 +17,39 @@ for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function() {
     const buttonValue = buttons[i].innerText;
 
-    // Checking if the button clicked is a number
+    // Checking if the button clicked is a number or decimal point
     if (!isNaN(buttonValue) || buttonValue === '.') {
-
-      // Checking if an operator has not been selected
-      if (operator === '') {
-        firstNumber += buttonValue;
-        updateDisplay(firstNumber);
-      } else {
-        secondNumber += buttonValue;
-        updateDisplay(secondNumber);
-      }
+      currentInput += buttonValue;
+      updateDisplay(storedInput + currentInput);
     } else {
 
       // Checking if the button clicked is an operator
-      if (buttonValue === '+' || buttonValue === '-' || buttonValue === 'x' || buttonValue === '/') {
-
-        // Setting the operator variable and updating the display
-        operator = buttonValue;
-        updateDisplay(operator);
+      if (buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/') {
+        if (previousResult !== '') {
+          storedInput = previousResult + buttonValue;
+          previousResult = '';
+        } else {
+          storedInput += currentInput + buttonValue;
+        }
+        currentInput = '';
+        updateDisplay(storedInput);
       } else if (buttonValue === '=') {
-        calculate();
+        storedInput += currentInput;
+        currentInput = '';
+        previousResult = eval(storedInput);
+        updateDisplay(previousResult);
+        storedInput = '';
       } else if (buttonValue === 'C') {
         // Clear the variables and update the display
-        firstNumber = '';
-        secondNumber = '';
-        operator = '';
+        currentInput = '';
+        storedInput = '';
+        previousResult = '';
         updateDisplay('');
+      }
+      else if (buttonValue === 'DEL') {
+        // Delete the last character from the current input
+        currentInput = currentInput.slice(0, -1);
+        updateDisplay(storedInput + currentInput);
       }
     }
   });
